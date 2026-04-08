7060ad23c8f0c6398b61a3a6c1d880c3337e21e9
Lucas <lucas@tmax.com.br>
2026-04-08 11:57:10 -0300

    Ajuste token provider

--- src/Auth/TokenProvider.php
+++ src/Auth/TokenProvider.php
@@ -11,18 +11,34 @@ final class TokenProvider implements TokenProviderInterface
     /** @var array|null */
     private $cachedToken;
 
-    public function __construct(Config $config, $token)
+    public function __construct(Config $config, $refreshToken)
     {
         $this->config = $config;
         $this->http   = new HttpClient($config->getBaseUrl(), [
             'Content-Type' => 'application/json',
         ]);
 
+        $authHeader = 'Basic ' . base64_encode($this->config->getClientId() . ':' . $this->config->getClientSecret());
+        $token = $this->http->request('POST', $this->config->getOauthTokenUrl(), [
+            'headers' => [
+                'Authorization' => $authHeader,
+                'Content-Type'  => 'application/x-www-form-urlencoded',
+            ],
+            'form_params' => [
+                'grant_type' => 'refresh_token',
+                'refresh_token' => $refreshToken,
+            ],
+        ]);
+
+        if (!is_array($token) || empty($token['access_token'])) {
+            throw new AuthException('Token inválido em client_credentials');
+        }
+        $token['_obtained_at'] = time();
         $this->cachedToken = $token;
     }
 
-    public function getToken(): array
+    public function getToken() :  array
     {
-        return ["access_token" => $this->cachedToken];
+        return $this->cachedToken;
     }
 }
